fb94449e02d500b81c98549afe6192d60d673ecd
Alerrando Breno <alerrando2@gmail.com>
2022-10-30 21:19:53 -0300

    feat(API): chamando a API

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useState } from "react";
+import axios from 'axios'
 
 export function App() {
+  const [locale, setLocale] = useState<string>("");
+  const [infoLocale, setInfoLocale] = useState<object>({})
 
   return (
     <div className="">
-        <h1>Hello World</h1>
+      <form>
+        <label>Cidade</label>
+        <input
+          type="text"
+          className="border-black"
+          onChange={(e) => setLocale(e.target.value)}
+          placeholder="Digite uma localidade"
+        />
+        <button type="button" onClick={() => handleClickWeather()}>Enviar</button>
+      </form>
     </div>
-  )
-}
+  );
 
+  function handleClickWeather(){
+    axios.get(`http://api.weatherapi.com/v1/current.json?key=b38973d27e114306810224449223010&q=${locale}`)
+    .then((res) => setInfoLocale(res.data))
+    console.log(infoLocale);
+  }
+}
