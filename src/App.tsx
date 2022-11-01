56df4ca53603ed5435564decf6347d47a0cdb649
Alerrando Breno <alerrando2@gmail.com>
2022-10-31 21:18:38 -0300

    feat(css): melhorando css

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,22 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 export function App() {
   const [locale, setLocale] = useState<string>("");
   const [infoLocale, setInfoLocale] = useState<object>({});
   const [date, setDate] = useState(new Date());
+  let background:string = bodyBackground();
+
 
   return (
-    <main
-      className={`h-screen w-full flex items-center justify-center ${bodyBackground()} bg-cover`}
-    >
-      <div className="w-[60%] md:w-5/12 h-3/5 bg-zinc-600">
+    <main className={`h-screen w-full flex items-center justify-center bg-cover`}>
+      <img src={background} className="absolute z-0 w-full h-full" alt="" />
+      <div className="w-[60%] md:w-5/12 h-3/5 bg-zinc-600 z-50">
         <div className="my-8 mx-8">
           <div className="grid gap-4 md:flex md:items-center md:justify-between">
             <input
               type="text"
-              className="w-full md:w-[80%] border-b-white border-solid border-b-2 bg-transparent"
+              className="w-full md:w-[80%] text-white border-b-white border-solid border-b-2 bg-transparent outline-none"
               onChange={(e) => setLocale(e.target.value)}
               placeholder="Digite uma localidade"
             />
@@ -33,18 +34,18 @@ export function App() {
       .get(
         `http://api.weatherapi.com/v1/forecast.json?key=b38973d27e114306810224449223010&q=${locale}&days=7&aqi=no&alerts=no`
       )
-      .then((res) => setInfoLocale(res.data));
+      .then((res) => setInfoLocale(res));
     console.log(infoLocale);
   }
 
   function bodyBackground() {
     const hora = date.getHours();
     if (hora >= 5 && hora <= 12) {
-      return "bg-[url('manha.jpg')]";
+      return "manha.jpg";
     } else if (hora > 12 && hora <= 17) {
-      return "bg-[url('tarde.jpg')]";
+      return "tarde.jpg";
     } else {
-      return "bg-[url('noite.jpg')]";
+      return "noite.jpg";
     }
   }
 }
