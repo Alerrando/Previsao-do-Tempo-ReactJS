6a2816aa8448c930a236ddab01e238428541828b
Alerrando Breno <alerrando2@gmail.com>
2022-11-04 14:13:55 -0300

    feat(css): melhorando o css

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -7,7 +7,7 @@ type WeatherProps = {
 
 export function Weather(props: WeatherProps) {
   const { infoLocale } = props;
-  const diaSemana:any = ["Domingo", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabádo"];
+  const diaSemana:any = ["", "Segunda-Feira", "Terça-Feira", "Quarta-Feira", "Quinta-Feira", "Sexta-Feira", "Sabádo", "Domingo"];
 
   console.log(infoLocale.current.condition)
   console.log(infoLocale);
@@ -15,22 +15,22 @@ export function Weather(props: WeatherProps) {
   return (
     <>
       <div className="w-full md:h-full block md:flex md:items-center md:justify-between">
-        <div className="grid gap-2 md:h-32 w-full md:w-auto font-sans py-4 px-4 shadow-mini-modal">
-          <div className="block md:grid gap-4 md:gap-0">
-            <div className="w-full md:w-auto flex items-center justify-between gap-7 md:gap-10">
-              <h2 className="text-xl md:text-2xl">{infoLocale.location.name}</h2>
+        <div className="grid gap-2 md:h-28 w-full md:w-auto font-sans py-4 px-4 shadow-mini-modal">
+          <div className="grid gap-2">
+            <div className="w-full md:w-auto md:flex items-center justify-between gap-7 md:gap-10">
+              <h2 className="text-xl md:text-2xl">{`${infoLocale.location.name} - ${infoLocale.location.country}`}</h2>
               <h2 className="text-2xl">{`${infoLocale.current.temp_c}°`}</h2>
             </div>
             <div className="w-full flex items-center justify-between">
-              <span className="text-sm md:mr-1">{diaSemana[moment().format("E")]}</span>
-              <span className="text-sm">{moment().format("HH:mm")}</span>
+              <span className="text-base">{moment().format("HH:mm")}</span>
             </div>
           </div>
-          <p className="text-sm">{infoLocale.current.condition.text}</p>
+          <p className="text-base">{infoLocale.current.condition.text}</p>
         </div>
 
-        <div className="py-4 px-4 md:h-36 w-full md:w-40 shadow-mini-modal flex flex-col items-center justify-center">
-          <img className="w-28 h-28 bg-cover" src={infoLocale.current.condition.icon} alt={infoLocale.current.condition.text} />
+        <div className="py-4 px-4 md:h-28 w-full md:w-40 shadow-mini-modal flex flex-col items-center justify-center">
+          <img className="w-18 h-18 bg-cover" src={infoLocale.current.condition.icon} alt={infoLocale.current.condition.text} />
+          <h2>{diaSemana[moment().format("E")]}</h2>
         </div>
       </div>
 
@@ -44,11 +44,14 @@ export function Weather(props: WeatherProps) {
         {infoLocale.forecast.forecastday.map((day:any, index:number) => (
           <>
             {index != 0 ? (
-                <div className="w-auto md:block flex text-center shadow-lg" key={index}>
-                  <img className="mx-auto" src={day.day.condition.icon} alt={day.day.condition.text} />
-                  <div className="flex items-center justify-between">
-                    <span className="opacity-50 pr-2">{`${day.day.maxtemp_c}°`}</span>
-                    <span>{`${day.day.mintemp_c}°`}</span>
+                <div className="w-auto md:block flex items-center justify-between text-center shadow-lg" key={index}>
+                  <img className="md:mx-auto" src={day.day.condition.icon} alt={day.day.condition.text} />
+                  <div className="block pr-4 md:pr-0">
+                    <div className="flex items-center justify-between">
+                      <span className="opacity-50 pr-2">{`${day.day.maxtemp_c}°`}</span>
+                      <span>{`${day.day.mintemp_c}°`}</span>
+                    </div>
+                    <p className="font-xs">{diaSemana[moment(day.date).format("E")]}</p>
                   </div>
                 </div>
             ): null}
